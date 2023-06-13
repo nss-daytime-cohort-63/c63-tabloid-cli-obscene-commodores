6e31cdb87e9bfb92e157354403ac20750721056b
Michael Stewart <michaelpstewart@comcast.net>
2023-06-13 11:58:08 -0500

    Menu Fix, Blog Boilerplate

---
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class BlogManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
+
+        public BlogManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _connectionString = connectionString;
+        }
+        public IUserInterfaceManager Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
