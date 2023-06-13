61b4f10b12584f6ad9705222631f646237e8f8d3
Nic <nic.lahde@gmail.com>
2023-06-13 12:56:48 -0500

    added post menu and uncommented option from main menu

--- TabloidCLI/UserInterfaceManagers/PostManager.cs
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    class Class1
+    public class PostManager : IUserInterfaceManager
     {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private string _connectionString;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
